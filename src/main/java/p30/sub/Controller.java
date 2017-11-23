5af536df6c6261bcb1286e48f9b115a13c103354
SteefP <s.pelgrom@hotmail.com>
2017-11-23 14:12:17 +0100

    bean outcommented

--- src/main/java/p30/sub/Controller.java
+++ src/main/java/p30/sub/Controller.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 public class Controller {
 	
 	
-	@Autowired @Qualifier(value = "AView")
+	@Autowired @Qualifier(value = "TheView")
  	View view1;
 	
 	
